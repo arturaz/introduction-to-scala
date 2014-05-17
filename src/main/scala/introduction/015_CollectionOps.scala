79819d3f9ae87e19de47c5e2e5a6e1fd6c402f9f
Arturas Slajus <x11@arturaz.net>
2014-05-17 13:34:22 +0300

    CollectionOps.

---
+++ src/main/scala/introduction/015_CollectionOps.scala
@@ -0,0 +1,32 @@
+package introduction
+
+object Main015 extends App {
+  val seq = Seq.tabulate(20)(identity)
+
+  println(seq.map(_ * 3))
+  println(seq.map(i => Seq(i, i * 2, i * 3)))
+  println(seq.map(i => Seq(i, i * 2, i * 3).flatten))
+  println(seq.flatMap(i => Seq(i, i * 2, i * 3)))
+
+  println(seq.reduce(_ + _))
+  println(seq.reduce(_ - _))
+  println(seq.reduceLeft(_ - _))
+  println(seq.reduceRight(_ - _))
+
+  println(seq.fold(-100)(_ + _))
+  println(seq.foldLeft(-100)(_ + _))
+  println(seq.foldLeft(Map.empty[Int, String]) {
+    case (map, int) => map updated (int, int.toString)
+  })
+
+  seq.zip(seq.reverse).foreach(println)
+  seq.reverse.zipWithIndex.foreach(println)
+  seq.sliding(2).foreach(println)
+  seq.zipAll(Seq(1, 2, 3), -1, -1).foreach(println)
+
+  seq.grouped(3).foreach(println)
+  seq.groupBy(_ % 2 == 0).foreach(println)
+
+  seq.permutations.foreach(println)
+  seq.combinations(2).foreach(println)
+}
